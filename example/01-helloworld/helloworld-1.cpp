b66838d8f1f2fffa1a5317183d3bd607416da3cf
Sarayu <sarayu.noo@gmail.com>
2023-05-29 16:08:19 +0700

    Update 2566-05-29

--- example/01-helloworld/helloworld-1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() //<= int main () { body }
-{ 
-    std::cout << "Hello World" << std::endl;
-    return 0;
-}
--- example/01-helloworld/helloworld-3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char *argv[]) //<= int main (int argc, char *argv[]) { body }
-{
-    cout << "Hello World" << endl;
-    return 0;
-}
